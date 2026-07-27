18d945fd28a8a0905e6822766fedaeed39e97e5f
vika oneil <oneilcode111@gmail.com>
2026-07-27 19:27:10 +1000

    init: start portfolio project

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center">
+      <h1 className="text-4xl font-bold">OneilCode | Frontend Developer</h1>
+    </main>
+  );
+}
