15708b0380bdaafe60b27d140768605c46769a07
vika oneil <oneilcode111@gmail.com>
2026-08-25 17:28:28 +1000

    chore: update Next.js config and proxy middleware

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,8 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 
 const withNextIntl = createNextIntlPlugin('./i18n/request.ts');
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const nextConfig: NextConfig = {/* config options here */};
 
 export default withNextIntl(nextConfig);
