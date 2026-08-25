8dc74262481a0bda00c8807440eb900aa59c242a
vika oneil <oneilcode111@gmail.com>
2026-08-25 17:28:09 +1000

    feat: update constants exports

--- constants/index.ts
+++ constants/index.ts
@@ -1,3 +1,6 @@
+import { Mail, Briefcase } from 'lucide-react';
+import { FaGithub, FaLinkedin, FaTelegram, FaWhatsapp } from 'react-icons/fa';
+
 export const TECHNOLOGIES = [
   'JavaScript',
   'TypeScript',
@@ -16,4 +19,43 @@ export const TECHNOLOGIES = [
   'Jest',
   'Cypress',
   'Zod',
+];
+
+export const CONTACTS = [
+  {
+    icon: FaTelegram,
+    label: 'Telegram',
+    href: 'https://t.me/vikaoneil',
+    color: 'hover:text-blue-400',
+  },
+  {
+    icon: FaWhatsapp,
+    label: 'WhatsApp',
+    href: 'https://wa.me/79990876564',
+    color: 'hover:text-green-500',
+  },
+  {
+    icon: FaLinkedin,
+    label: 'LinkedIn',
+    href: 'https://linkedin.com/in/viktoriia-o-neil-956167228',
+    color: 'hover:text-blue-600',
+  },
+  {
+    icon: FaGithub,
+    label: 'GitHub',
+    href: 'https://github.com/oneilcode',
+    color: 'hover:text-gray-900 dark:hover:text-white',
+  },
+  {
+    icon: Mail,
+    label: 'Email',
+    href: 'mailto:oneilcode111@gmail.com',
+    color: 'hover:text-red-500',
+  },
+  {
+    icon: Briefcase,
+    label: 'Habr Career',
+    href: 'https://career.habr.com/vikaoneil',
+    color: 'hover:text-orange-500',
+  },
 ];
